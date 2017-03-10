d6ba7e7d0571936ec4344ad3859d3a9f2aca67be
Mathias Rav <rav@cs.au.dk>
2017-03-10 16:59:21 -0500

    Add copy_journal.py

---
+++ copy_journal.py
@@ -0,0 +1,19 @@
+import argparse
+from systemd_journal import journal_open
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('output')
+    args = parser.parse_args()
+
+    fin = journal_open(args.input, 'r')
+    fout = journal_open(args.output, 'x+')
+    for e in fin:
+        print(fout.append_entry(e))
+    fout.close()
+
+
+if __name__ == '__main__':
+    main()
