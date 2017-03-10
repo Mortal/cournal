efcd5b8e510cfaee4ed0a1d7960fe7bce5c6e301
Mathias Rav <rav@cs.au.dk>
2017-03-10 09:42:17 -0500

    systemd 232 API

--- systemd_journal.py
+++ systemd_journal.py
@@ -1,13 +1,12 @@
 from ctypes import byref
-from journal_file import journal_file_open, POINTER, JournalFile
+from journal_file import journal_file_open, POINTER, JournalFile, String
 
 
 def main():
     from os import O_RDWR, O_CREAT
 
     ret = POINTER(JournalFile)()
-    systemd_shared = ctypes.cdll.LoadLibrary('/usr/lib/systemd/libsystemd-shared-232.so')
-    assert not journal_file_open("test.journal", O_RDWR|O_CREAT, 0o666, True, True, None, None, None, byref(ret))
+    assert not journal_file_open(-1, String("test.journal"), O_RDWR|O_CREAT, 0o666, True, True, None, None, None, None, byref(ret))
     print(ret)
 
 
