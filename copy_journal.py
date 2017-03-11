4e011abe8d7827a1eaba38fb5bdbf0880b90c653
Mathias Rav <rav@cs.au.dk>
2017-03-11 11:01:34 -0500

    Add --remove-unit

--- copy_journal.py
+++ copy_journal.py
@@ -4,15 +4,25 @@ from systemd_journal import journal_open
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--remove-unit')
     parser.add_argument('input')
     parser.add_argument('output')
     args = parser.parse_args()
 
     fin = journal_open(args.input, 'r')
     fout = journal_open(args.output, 'x+')
+    remove_items = (['_SYSTEMD_UNIT=%s' % args.remove_unit]
+                    if args.remove_unit else [])
+    remove_items = [i.encode() for i in remove_items]
+    skipped = written = 0
     for e in fin:
-        print(fout.append_entry(e), e.items)
+        if any(i in e.raw_items for i in remove_items):
+            skipped += 1
+            continue
+        written += 1
+        fout.append_entry(e)
     fout.close()
+    print("Wrote %s entries and skipped %s entries" % (written, skipped))
 
 
 if __name__ == '__main__':
